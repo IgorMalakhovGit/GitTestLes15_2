62217e52b98df6196333bf494a40d6b7dcd2aacc
IgorMalakhovGit <gittest2991@gmail.com>
2022-09-18 22:43:58 +0600

    TEst console1

---
+++ src/test/java/guru/qa/ExampleTest1.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest1 {
+
+    @Test
+    void ExampleTest1(){
+        Assertions.assertTrue(2>1);
+
+    }
+}
